a14030ad36908e9bf81edf46ce8cbb59aaca92ad
madzdz <31445572+madzdz@users.noreply.github.com>
2020-04-21 14:25:53 +0100

    fix mobile navigation

--- script/fetch.js
+++ script/fetch.js
@@ -1,4 +1,5 @@
-fetch("./sections/nav.html")
+function loadSections() {
+  fetch("./sections/nav.html")
   .then(response => {
     return response.text()
   })
@@ -21,4 +22,37 @@ fetch("./sections/footer.html")
   .then(data => {
     document.querySelector("footer").innerHTML = data;
   });
-  
+}
+
+function navShow() {
+  const openNavIcon = document.querySelector('#open-nav');
+  const closeNavIcon = document.querySelector('#close-nav');
+  const menu = document.querySelector('.nav__box');
+  const background = document.querySelector('.nav__background');
+
+  openNavIcon.addEventListener('click', function(){
+      menu.style.right='0';
+      background.style.zIndex='2';
+      background.style.opacity='1';
+      document.querySelector('body').style.overflowY= 'hidden';
+
+  });
+
+  closeNavIcon.addEventListener('click', function(){
+      menu.style.right='-68%';
+      background.style.zIndex='-1';
+      background.style.opacity='0';
+      document.querySelector('body').style.overflowY= 'visible';
+  });
+}
+
+loadSections();
+checkVariable();
+
+function checkVariable() {
+  if ( document.querySelector('.hamburger') != null ) {
+    navShow();
+  } else {
+    window.setTimeout("checkVariable();",100);
+  }
+} 
--- script/nav.js
+++ script/nav.js
@@ -1,19 +1,2 @@
-const openNavIcon = document.querySelector('#open-nav');
-const closeNavIcon = document.querySelector('#close-nav');
-const menu = document.querySelector('.nav__box');
-const background = document.querySelector('.nav__background');
 
-openNavIcon.addEventListener('click', function(){
-    menu.style.right='0';
-    background.style.zIndex='2';
-    background.style.opacity='1';
-    document.querySelector('body').style.overflowY= 'hidden';
-
-});
-
-closeNavIcon.addEventListener('click', function(){
-    menu.style.right='-68%';
-    background.style.zIndex='-1';
-    background.style.opacity='0';
-    document.querySelector('body').style.overflowY= 'visible';
-});
+    
