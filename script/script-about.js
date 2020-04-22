c19058ced053bf5599afe1114d262430a85b60f8
madzdz <31445572+madzdz@users.noreply.github.com>
2020-04-22 14:46:47 +0100

    fixed background images' positions

--- script/script-about.js
+++ script/script-about.js
@@ -4,6 +4,7 @@ const backSide = document.querySelectorAll('.director__side--back');
 
 btns.forEach((btn,i)=>{
     btn.addEventListener('click', function(){
+
         btn.classList.toggle('btn--back');
         btn.classList.toggle('btn--more');
         frontSide[i].classList.toggle('show-front');
